d7b62b0d771de91ea1d9d59e2d8dad561140b0d8
AMA-434 <89720399+AMA-434@users.noreply.github.com>
2021-12-16 16:45:00 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,13 @@
+cuteBot.stopcar()
 radio.setGroup(7)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.AB)) {
         radio.sendValue("double", 50)
-        basic.showArrow(ArrowNames.North)
-    } else if (input.buttonIsPressed(Button.B)) {
-        radio.sendValue("right", 25)
-        basic.showArrow(ArrowNames.East)
     } else if (input.buttonIsPressed(Button.A)) {
+        radio.sendValue("right", 25)
+    } else if (input.buttonIsPressed(Button.B)) {
         radio.sendValue("left", 25)
-        basic.showArrow(ArrowNames.West)
     } else {
         radio.sendNumber(0)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # # # .
-            . . . . .
-            . . . . .
-            `)
     }
 })
