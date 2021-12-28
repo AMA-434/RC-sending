2caf027acd49646760150a28443bec0a3be37b3f
AMA-434 <89720399+AMA-434@users.noreply.github.com>
2021-12-28 14:57:53 +0900

    ライントレースセンサーを使ってバックができるようにしました

--- main.ts
+++ main.ts
@@ -7,6 +7,12 @@ basic.forever(function () {
         radio.sendValue("right", 66)
     } else if (input.buttonIsPressed(Button.B)) {
         radio.sendValue("left", 66)
+    } else if (cuteBot.tracking(cuteBot.TrackingState.L_R_unline)) {
+        radio.sendValue("double", -40)
+    } else if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+        radio.sendValue("right", -33)
+    } else if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+        radio.sendValue("left", -33)
     } else {
         radio.sendNumber(0)
     }
